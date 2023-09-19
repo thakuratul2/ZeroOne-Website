88c76b1246c47ca20e3878b4edbb8be32e9c1a17
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-19 22:25:19 +0530

    Update

--- assets/js/page.js
+++ assets/js/page.js
@@ -7774,3 +7774,18 @@ parcelRequire = (function (t, e, i, n) {
   {},
   ["C3Xv"]
 );
+let panelsElement = document.querySelectorAll('.panel');
+
+
+let removeActiveClasses = () => {
+    panelsElement.forEach(panel => {
+        panel.classList.remove('active');
+    });
+};
+
+panelsElement.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
